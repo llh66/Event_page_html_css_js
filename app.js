23229b8fab7be5626519590356e2d0f4502d8193
davidHuyDang <davidhuydang02@gmail.com>
2024-09-17 20:20:33 -0400

    Update app.js (#3)

--- app.js
+++ app.js
@@ -1,3 +1,27 @@
+//Made an array of artists 
+const artists = [
+    { picture: './images/artistImage0.jpeg', name: "ZAQ", date: "Sunday August 11th, 2024", location: "EXHIBITION HALL C - #136", time: "4:00 PM - 5:00PM", Genre: "Dance/Electronic/ J-POP" },
+    { picture: './images/artistImage1.jpeg', name: "TRUE", date: "Sunday August 11th, 2024", location: "EXHIBITION HALL C - AUTH 2", time: "2:30 PM - 3:30PM", Genre: "J-POP" },
+    { picture: './images/artistImage2.jpeg', name: "MADKID", date: "Saturday August 10th, 2024", location: "EXHIBITION HALL C - BLUE", time: "7:00 PM - 9:00M", Genre: "Dubstep/Hip-Hop" },
+]
+//loop to grab info on each artists 
+for (let i = 0; i < artists.length; i++) {
+    //outputs the information on html
+    document.querySelector("#artistInfo").innerHTML +=
+        `<div id ="artistContainer">
+        <div id = "artistPicture">
+        <img src=${artists[i].picture} />
+        </div>
+        <div = "artistAbout">
+        <p>Name: ${artists[i].name}</p> 
+        <p>Date: ${artists[i].date}</p>  
+        <p>Location: ${artists[i].location}</p> 
+       <p> Time: ${artists[i].time}</p> 
+        <p>Genre: ${artists[i].Genre}</p> 
+        </div>
+        </div>
+        `
+}
 
 
 
